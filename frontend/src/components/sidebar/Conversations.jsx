3d8ef0a5d2b72b31b75eede602c30868c31001f3
Ashutosh Pal <Ashutoshpal3179@gmail.com>
2024-07-16 18:50:55 +0530

    2nd part completed - ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+       <Conversation/>
+       <Conversation/>
+       <Conversation/>
+       <Conversation/>
+       <Conversation/>
+       <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
+
+
+
+//STARTER CODE FOR SNIPPIT
+
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//        <Conversation/>
+//        <Conversation/>
+//        <Conversation/>
+//        <Conversation/>
+//        <Conversation/>
+//        <Conversation/>
+//     </div>
+//   )
+// }
+
+// export default Conversations
+
